4e1c1513c1d8a992fd7087a51344b7806c03a33b
hkmast <kms970910@naver.com>
2023-05-19 10:53:23 +0900

    feat: elasticsearch 로 사용 및 정리

--- haystack_app/service/models/bm25_search.py
+++ haystack_app/service/models/bm25_search.py
@@ -1,50 +1,41 @@
 import os
-from haystack.document_stores import ElasticsearchDocumentStore
-from haystack.nodes import DensePassageRetriever, TextConverter
+from haystack.document_stores import ElasticsearchDocumentStore, InMemoryDocumentStore
+from haystack.nodes import DensePassageRetriever, TextConverter, PreProcessor, BM25Retriever
 from haystack.pipelines import DocumentSearchPipeline
 from haystack import Pipeline
 from haystack.utils import convert_files_to_docs
-import os
-from haystack.document_stores import ElasticsearchDocumentStore, InMemoryDocumentStore
-from haystack.nodes import TextConverter, PreProcessor, BM25Retriever
+from haystack.nodes import TextConverter
 from haystack.pipelines import DocumentSearchPipeline, SearchSummarizationPipeline
 from haystack import Pipeline
+import logging
 
+# 지정한 위치에 있는 데이터를 받아옴
 print("now dir ls = ", os.listdir("."))
 doc_dir = "./haystack_app/law_data"
 
-# # Elasticsearch connection and set DocumentStore
-# host = os.environ.get("ELASTICSEARCH_HOST", "localhost")
-# document_store = ElasticsearchDocumentStore(
-#     host=host,
-#     username="",
-#     password="",
-#     index="bm25"
-# )
+# Elasticsearch를 document_store에 연결
+document_store = ElasticsearchDocumentStore(
+    host="elasticsearch_for_haystack_app",  # local 환경 에서는 "localhost"
+    username="",
+    password="",
+    index="bm25_search" # 인덱스 설정
+)
 
-document_store = InMemoryDocumentStore(use_bm25=True, bm25_algorithm="BM25Plus")
-
-# Convert files to dictionaries for indexing
+# 파일들을 딕셔너리 형태로 변환하여 색인
 docs = convert_files_to_docs(dir_path=doc_dir, clean_func=None)
-
-# Write documents to DocumentStore
 document_store.write_documents(docs)
 
-# Set BM25Retriever
+# BM25 Retriever 설정
 retriever = BM25Retriever(document_store=document_store)
 
-# Set DocumentSearchPipeline
+# Document Search Pipeline 설정
 pipeline = DocumentSearchPipeline(retriever)
-#
+
+# 검색 함수
 def search(query, k):
     return pipeline.run(query, params={"Retriever": {"top_k": int(k)}})
 
-if __name__ == "__main__":
-    # print(search("매수신고인", 3)['documents'])
-    bm = search("매수신고인", 3)['documents']
-    print(bm)
-    
-    all_list = [i.meta['name'] for i in bm]
 
-    print(all_list)
-    
+# 테스트
+if __name__ == "__main__":
+    print(search("고속도로", 3)["documents"].meta)
