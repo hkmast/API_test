6549e9cef7d104ac37edf6765168f13ccbad8cfa
hkmast <kms970910@naver.com>
2023-05-19 09:46:44 +0900

    feat: 필요없는 부분 정리, 주석 정리

--- haystack_app/service/models/sbert_search.py
+++ haystack_app/service/models/sbert_search.py
@@ -1,24 +1,19 @@
 import os
 from haystack.document_stores import ElasticsearchDocumentStore
-from haystack.nodes import DensePassageRetriever, TextConverter
+from haystack.nodes import DensePassageRetriever
 from haystack.pipelines import DocumentSearchPipeline
-from haystack import Pipeline
 from haystack.utils import convert_files_to_docs
-import os
-from haystack.document_stores import ElasticsearchDocumentStore
-from haystack.nodes import TextConverter, PreProcessor, BM25Retriever
-from haystack.pipelines import DocumentSearchPipeline, SearchSummarizationPipeline
-from haystack import Pipeline
 
+# 지정한 위치에 있는 데이터를 받아옴
 print("now dir ls = ", os.listdir("."))
 doc_dir = "./haystack_app/law_data"
 
-# elasticsearch connection and set document store
+# Elasticsearch를 document_store에 연결
 document_store = ElasticsearchDocumentStore(
-    host="elasticsearch_for_haystack_app",
+    host="elasticsearch_for_haystack_app",  # local 환경 에서는 "localhost"
     username="",
     password="",
-    index="sbert"
+    index="sbert_search",
 )
 
 # 파일들을 딕셔너리 형태로 변환하여 색인
@@ -27,22 +22,25 @@ document_store.write_documents(docs)
 
 
 # Dense Passage Retriever 설정
-retriever = DensePassageRetriever(document_store=document_store,
-                                 query_embedding_model= "sentence-transformers/paraphrase-multilingual-mpnet-base-v2",
-                                 passage_embedding_model= "sentence-transformers/paraphrase-multilingual-mpnet-base-v2",
-                                 use_gpu=False)
+retriever = DensePassageRetriever(
+    document_store=document_store,
+    query_embedding_model="sentence-transformers/paraphrase-multilingual-mpnet-base-v2",  # 다국어에 어울리는 임베딩
+    passage_embedding_model="sentence-transformers/paraphrase-multilingual-mpnet-base-v2",  # 다국어에 어울리는 임베딩
+    use_gpu=False,
+)
 
-
+# embedding_model에 맞춰서 임베딩
 document_store.update_embeddings(retriever)
 
 # Document Search Pipeline 설정
 pipeline = DocumentSearchPipeline(retriever)
 
 
-#
+# 검색 함수
 def search(query, k):
     return pipeline.run(query, params={"Retriever": {"top_k": int(k)}})
 
 
+# 테스트
 if __name__ == "__main__":
-    print(search("1", 3)['documents'].meta)
+    print(search("매수신고인", 3)["documents"].meta)
