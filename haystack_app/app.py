98d5dd385323cfdc45ebde3fd2b545de88e7c672
hkmast <kms970910@naver.com>
2023-05-20 13:47:01 +0900

    feat: 시간 출력 정리

--- haystack_app/app.py
+++ haystack_app/app.py
@@ -6,7 +6,7 @@ t = 300
 print("waiting elastic server on...")
 for i in range(t):
     time.sleep(1)
-    print(f"loadging time...{i+1}/{t}", end="")
+    print(f"loadging time...{i+1}/{t}")
 print("\rwaiting elastic server on done")
 
 print("start flask app...")
