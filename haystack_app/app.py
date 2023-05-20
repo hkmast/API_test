db94c9232babf2feaff9f5a4f515823a6cabb6a4
hkmast <kms970910@naver.com>
2023-05-20 15:49:56 +0900

    feat: 로딩시간 줄임

--- haystack_app/app.py
+++ haystack_app/app.py
@@ -1,13 +1,13 @@
 from service import create_app
 import time
 
-t = 300
+t = 60
 
 print("waiting elastic server on...")
 for i in range(t):
     time.sleep(1)
     print(f"loadging time...{i+1}/{t}")
-print("\rwaiting elastic server on done")
+print("waiting elastic server on done")
 
 print("start flask app...")
 create_app().run(port=7777, debug=False)
